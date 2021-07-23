b43e6a26c9211f16cfb8523876f7e325c45cfa35
Almaas <almaasfakhrurozi763@gmail.com>
2021-07-23 09:47:16 +0700

    Add project files.

---
+++ WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get(string cityName)
+        {
+            var URL = $"http://api.weatherapi.com/v1/current.json?key=e86611a19aca4cc5bf121507212307&q={cityName}&aqi=no";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(URL);
+            return await response.Content.ReadAsStringAsync();
+
+        }
+    }
+}
